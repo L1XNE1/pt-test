b6f0c3f3ab203bf38de1398e00f2a953dae63a0f
l1xne1 <lvxfmv@yandex.ru>
2023-08-20 16:40:20 +0300

    Добавил тест для get_issues_of_repo

--- tests/test_githubissues.py
+++ tests/test_githubissues.py
@@ -35,3 +35,28 @@ def test_get_list_of_repos():
 
     assert output == expected_output
     assert output != bad_output
+
+
+@responses.activate
+def test_get_issues_of_repo():
+    with open('tests/data/devopshq-crosspm-issues.json', 'r') as f:
+        test_input = f.read()
+
+    responses.get(
+        'https://api.github.com/repos/devopshq/crosspm/issues',
+        json=json.loads(test_input),
+    )
+
+    g = GitHubIssues('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')
+    output = g.get_issues_of_repo('devopshq', 'crosspm')
+
+    expected_output = [
+        '123: Altsearch',
+        '122: [pre-commit.ci] pre-commit autoupdate'
+    ]
+
+    output.sort()
+    expected_output.sort()
+
+    assert output == expected_output
+
