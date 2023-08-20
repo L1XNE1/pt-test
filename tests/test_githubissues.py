4959ee90d9049ac8c5687fa25f8582551dd02a05
Lev <45142197+L1XNE1@users.noreply.github.com>
2023-08-20 17:15:24 +0300

    Update test_githubissues.py

--- tests/test_githubissues.py
+++ tests/test_githubissues.py
@@ -2,22 +2,22 @@ import pytest
 import json
 import responses
 from src.githubissues import GitHubIssues
+#каждая функция ниже это отдельный тест
 
-
-@responses.activate
+@responses.activate #подделываем ответы для модуля requests (офлайн тест)
 def test_get_list_of_repos():
-    with open('tests/data/devopshq-repos.json', 'r') as f:
+    with open('tests/data/devopshq-repos.json', 'r') as f: #читает файл с тестовыми данными
         test_input = f.read()
 
-    responses.get(
+    responses.get( # подделываем get запрос
         'https://api.github.com/orgs/devopshq/repos',
         json=json.loads(test_input),
     )
 
     g = GitHubIssues('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')
-    output = g.get_list_of_repos('devopshq')
+    output = g.get_list_of_repos('devopshq') #результат это список 
 
-    expected_output = [
+    expected_output = [ # тут руками написано что ждем
         'crosspm',
         'vspheretools',
         'zabbix-youtrack-action'
@@ -29,11 +29,12 @@ def test_get_list_of_repos():
         'zabb3ix-youtrack-action'
     ]
 
+    #ниже сортируются все списки
     output.sort()
     expected_output.sort()
     bad_output.sort()
 
-    assert output == expected_output
+    assert output == expected_output #мы ждем что output должен быть равен тому, что мы ждем (выше)
     assert output != bad_output
 
 
@@ -61,7 +62,7 @@ def test_get_issues_of_repo():
     assert output == expected_output
 
 
-@responses.activate
+@responses.activate #4 разных запроса (подделывает на 4 разных запроса)
 def test_get_issues_of_org():
     with open('tests/data/devopshq-repos.json', 'r') as f:
         test_input = f.read()
@@ -94,7 +95,7 @@ def test_get_issues_of_org():
     g = GitHubIssues('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')
     output = g.get_issues_of_org('devopshq')
 
-    expected_output = [
+    expected_output = [ #ожидаемый вывод написан руками
         {
             'repo': 'crosspm',
             'issues': [
@@ -118,5 +119,5 @@ def test_get_issues_of_org():
         }
     ]
 
-    assert output == expected_output
+    assert output == expected_output #сравниваем что мы получили с ожидаемым выводом
 
